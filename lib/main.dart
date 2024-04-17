625c5fc117b058b955c5a260030ae6f3546dc760
Abin P S <abinps23@gmail.com>
2024-04-17 12:20:29 +0530

    simple web app with flutter and firebase

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:simple_web_app/pages/home_page.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ),
+  );
+}
